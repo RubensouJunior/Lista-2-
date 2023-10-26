f82e372ccb16a9cb9911afccb83a52a6107cb863
andreiaprofessora <148285282+andreiaprofessora@users.noreply.github.com>
2023-10-26 19:01:43 -0300

    Update exercicio10.js
    Inserção de um 0.

--- exercicio10.js
+++ exercicio10.js
@@ -1,7 +1,7 @@
 
 // exercicio 10
 const renda = prompt("Apresenta sua renda para analisar se deve pagar impostos: ");
-if (renda <= 2.640) {
+if (renda <= 2.6400) {
     console.log("Isento do imposto de renda");
 } else {
     console.log("Elegível para a taxa do imposto de renda");
